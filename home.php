9237a96c5244699d248d0d3ebb52c3b6ad995ad4
千果志 白水 <102590095+chikashishirozu@users.noreply.github.com>
2024-11-30 12:24:51 +0900

    Update home.php

--- home.php
+++ home.php
@@ -98,8 +98,8 @@ if (!isset($_SESSION['user_id'])) {
 	    color: blue;
 	}
 	#current-time {
-	    font-size: 24px;
-	    font-weight: bold;
+	    font-size: 20px;
+	    font-weight: 500;
 	    margin: 0 0 20px 0;
 	    color: #333;
 	    background-color: #eef;
